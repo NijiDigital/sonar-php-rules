97567b9f9093fdac5283c0bcdf8027e2aafe078b
Pierre-Loup TRISTANT <pierre-loup.tristant@niji.fr>
2018-10-04 12:33:15 +0200

    Updates CURLDisableTLSCheck implementation
    https://jira.sonarsource.com/browse/RSPEC-4831

--- src/main/java/fr/niji/sonarphp/checks/CURLDisableTLSCheck.java
+++ src/main/java/fr/niji/sonarphp/checks/CURLDisableTLSCheck.java
@@ -1,7 +1,13 @@
 package fr.niji.sonarphp.checks;
 
 import com.google.common.collect.ImmutableSet;
+import java.util.List;
 import org.sonar.check.Rule;
+import org.sonar.php.checks.utils.CheckUtils;
+import org.sonar.php.tree.visitors.AssignmentExpressionVisitor;
+import org.sonar.plugins.php.api.symbols.Symbol;
+import org.sonar.plugins.php.api.tree.CompilationUnitTree;
+import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.Tree.Kind;
 import org.sonar.plugins.php.api.tree.declaration.NamespaceNameTree;
 import org.sonar.plugins.php.api.tree.expression.ArrayInitializerFunctionTree;
@@ -14,67 +20,94 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Set;
 
-@Rule(key = CURLDisableTLSCheck.KEY )
+@Rule(key = CURLDisableTLSCheck.KEY)
 public class CURLDisableTLSCheck extends PHPVisitorCheck {
 
     public static final String KEY = "S2";
-    private static final String CURL_SETOPT_FUNCTION = "curl_setopt";
-    private static final Set<String> CURLOPT_SSL = ImmutableSet.of("CURLOPT_SSL_VERIFYHOST","CURLOPT_SSL_VERIFYPEER");
+    private static final String CURL_SETOPT = "curl_setopt";
+    private static final String CURLOPT_SSL_VERIFYHOST = "CURLOPT_SSL_VERIFYHOST";
+    private static final String CURLOPT_SSL_VERIFYPEER = "CURLOPT_SSL_VERIFYPEER";
+
+    private static final String MESSAGE = "Change this code to enable trust chain verification.";
+    private AssignmentExpressionVisitor assignmentExpressionVisitor;
+
+    @Override
+    public void visitCompilationUnit(CompilationUnitTree tree) {
+        assignmentExpressionVisitor = new AssignmentExpressionVisitor(context().symbolTable());
+        tree.accept(assignmentExpressionVisitor);
+        super.visitCompilationUnit(tree);
+    }
 
     @Override
     public void visitFunctionCall(FunctionCallTree tree) {
-        ExpressionTree callee = tree.callee();
+        String functionName = CheckUtils.getFunctionName(tree);
+        List<ExpressionTree> arguments = tree.arguments();
 
-        // Detect curl_setopt_function usage
+        // Detect curl_setopt function usage
         // http://php.net/manual/fr/function.curl-setopt.php
-        if (callee.is(Kind.NAMESPACE_NAME) && ((NamespaceNameTree) callee).qualifiedName().equals(CURL_SETOPT_FUNCTION)) {
-
-            // curl_setopt secound argument is the option key (CURLOPT_XXX)
-            // Detect option settings that disable TLS trust chain verification
-            ExpressionTree curlOptKey = tree.arguments().get(1);
-            if(curlOptKey.is(Kind.NAMESPACE_NAME) && CURLOPT_SSL.contains(((NamespaceNameTree) curlOptKey).qualifiedName())) {
-
-                // curl_setopt third argument for TLS options is a Boolean value
-                ExpressionTree curlOptValue = tree.arguments().get(2);
-
-                // Voir http://php.net/manual/fr/language.types.boolean.php
-                Boolean optDisabled = false;
-                Boolean optValueUnknown = false;
-                if(curlOptValue.is(Kind.BOOLEAN_LITERAL)) {
-                    // Detect false or FALSE boolean values
-                    Boolean value = Boolean.parseBoolean(((LiteralTree )curlOptValue).value());
-                    optDisabled = !value;
-                } else if (curlOptValue.is(Kind.NULL_LITERAL)) {
-                    // Detect NULL literal values
-                    optDisabled = true;
-                } else if (curlOptValue.is(Kind.NUMERIC_LITERAL)) {
-                    // Detect integer or floating literals equals to zero
-                    String value = ((LiteralTree )curlOptValue).value();
-                    try {
-                        optDisabled = NumberFormat.getInstance().parse(value).intValue() == 0;
-                    } catch (ParseException exception) {
-                        optValueUnknown = true;
-                    }
-                } else if (curlOptValue.is(Kind.REGULAR_STRING_LITERAL)) {
-                    // Detect empty string ("") on zero character string ("0")
-                    ImmutableSet emptyOrZeroCharStringLiteral = ImmutableSet.of("\"\"", "\"0\"");
-                    optDisabled = emptyOrZeroCharStringLiteral.contains(((LiteralTree )curlOptValue).value());
-                } else if (curlOptValue.is(Kind.ARRAY_INITIALIZER_FUNCTION)) {
-                    // Detect empty array literal
-                    optDisabled = ((ArrayInitializerFunctionTree) curlOptValue).arrayPairs().size() == 0;
-                } else {
-                    optValueUnknown = true;
-                }
-
-                if(optDisabled) {
-                    context().newIssue(this, callee, "TLS trust chain verification should not be disabled.");
-                } else if (optValueUnknown) {
-                    context().newIssue(this, callee, "Potential TLS trust chain misconfiguration.");
-                }
+        if (CURL_SETOPT.equals(functionName) && arguments.size() > 2) {
+            ExpressionTree optionArgument = arguments.get(1);
+            if (optionArgument.is(Tree.Kind.NAMESPACE_NAME) &&
+                    CURLOPT_SSL_VERIFYHOST.equals(((NamespaceNameTree) optionArgument).name().text())) {
+                checkCURLSSLVerifyHost(getAssignedValue(arguments.get(2)));
+            } else if (optionArgument.is(Tree.Kind.NAMESPACE_NAME) &&
+                    CURLOPT_SSL_VERIFYPEER.equals(((NamespaceNameTree) optionArgument).name().text())) {
+                checkCURLSSLVerifyPeer(getAssignedValue(arguments.get(2)));
             }
         }
 
         // super method must be called in order to visit function call node's children
         super.visitFunctionCall(tree);
+    }
+
+    private void checkCURLSSLVerifyHost(ExpressionTree curlOptValue) {
+
+        Boolean verifyHostEnabled = false;
+        if (curlOptValue.is(Kind.NUMERIC_LITERAL)) {
+            // Detect 2 integer value
+            String value = ((LiteralTree) curlOptValue).value();
+            verifyHostEnabled = (value == "2");
+        } else if (curlOptValue.is(Kind.REGULAR_STRING_LITERAL)) {
+            // Detect '2' or "2" string characters
+            ImmutableSet twoStringLiteral = ImmutableSet.of("\'2\'", "\"2\"");
+            verifyHostEnabled = twoStringLiteral.contains(((LiteralTree) curlOptValue).value());
+        }
+
+        if (!verifyHostEnabled) {
+            context().newIssue(this, curlOptValue, MESSAGE);
+        }
+    }
+
+    private void checkCURLSSLVerifyPeer(ExpressionTree curlOptValue) {
+
+        Boolean verifyPeerEnabled = false;
+        if (curlOptValue.is(Kind.BOOLEAN_LITERAL)) {
+            // Detect true or TRUE boolean values
+            ImmutableSet trueStringLiteral = ImmutableSet.of("true", "TRUE");
+            verifyPeerEnabled = Boolean.parseBoolean(((LiteralTree) curlOptValue).value());
+
+        } else if (curlOptValue.is(Kind.NUMERIC_LITERAL)) {
+            // Detect 1 integer value
+            String value = ((LiteralTree) curlOptValue).value();
+            verifyPeerEnabled = (value == "1");
+        } else if (curlOptValue.is(Kind.REGULAR_STRING_LITERAL)) {
+            // Detect '1' or "1" string characters
+            ImmutableSet twoStringLiteral = ImmutableSet.of("\'1\'", "\"1\"");
+            verifyPeerEnabled = twoStringLiteral.contains(((LiteralTree) curlOptValue).value());
+        }
+
+        if (!verifyPeerEnabled) {
+            context().newIssue(this, curlOptValue, MESSAGE);
+        }
+    }
+
+    private ExpressionTree getAssignedValue(ExpressionTree value) {
+        if (value.is(Tree.Kind.VARIABLE_IDENTIFIER)) {
+            Symbol valueSymbol = context().symbolTable().getSymbol(value);
+            return assignmentExpressionVisitor
+                    .getUniqueAssignedValue(valueSymbol)
+                    .orElse(value);
+        }
+        return value;
     }
 }
--- src/test/resources/checks/cURLDisableTLSCheck.php
+++ src/test/resources/checks/cURLDisableTLSCheck.php
@@ -1,20 +1,18 @@
 <?php
 
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // NOK {{TLS trust chain verification should not be disabled.}}
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, NULL); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, array()); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, ""); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, "0"); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0.0); // NOK
+$verify_host_on = '2'; // OK
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $verify_host_on);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '2'); // OK; default value is 2 to "check the existence of a common name and also verify that it matches the hostname provided" according to PHP's documentation
 
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // NOK
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // NOK
+$verify_peer_on = TRUE; // OK; default value is TRUE
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $verify_peer_on);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, '1'); // OK
 
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // OK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true); // OK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, "yes"); // OK
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 12); // OK
+$verify_host_off = FALSE; // NOK {{Change this code to enable trust chain verification.}}
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $verify_host_off);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, TRUE); // NOK; TRUE is casted to 1 which is not a secure configuration
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '0'); // NOK
 
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $a); // NOK {{Potential TLS trust chain misconfiguration.}}
+$verify_peer_off = '0'; // NOK
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // NOK
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $verify_peer_off);
